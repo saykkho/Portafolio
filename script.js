581ab7bbeb8fe91f4037c5e769d08ba2ce4b9d2c
saykkho <novaclimers@gmail.com>
2023-12-10 23:06:49 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+strings: [" Highschool Studient","Frontend Developer","Anime Watcher"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true
+})
